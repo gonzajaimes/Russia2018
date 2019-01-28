ec9f86db92ad01c635ed584bbd46934583bd6c9a
gjaimes <gjaimes@emeingenieria.com.co>
2019-01-28 15:23:22 -0500

    Notifications Console Program

---
+++ Russia2018.NotificationsTest/Program.cs
@@ -0,0 +1,33 @@
+﻿
+namespace Russia2018.NotificationsTest
+{
+    using Microsoft.Azure.NotificationHubs;
+    using System;
+
+    public class Program
+    {
+        private static NotificationHubClient hub;
+
+        public static void Main(string[] args)
+        {
+            hub = NotificationHubClient.CreateClientFromConnectionString("Endpoint=sb://russia2018.servicebus.windows.net/;" + 
+                                                                         "SharedAccessKeyName=DefaultFullSharedAccessSignature;" +
+                                                                         "SharedAccessKey=z/zPg9aiY3IMLfXpfKT86sNPIDSOdBHBK+UHT2rudZI=", "Russia2018");
+
+            do
+            {
+                Console.WriteLine("Type a new message:");
+                var message = Console.ReadLine();
+                SendNotificationAsync(message);
+                Console.WriteLine("The message was sent...");
+            } while (true);
+        }
+
+        private static async void SendNotificationAsync(string message)
+        {
+           await hub.SendFcmNativeNotificationAsync("{ \"data\" : {\"Message\":\"" + message + "\"}}");
+        }
+
+    }
+
+}
